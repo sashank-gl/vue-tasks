5e190be382f81690b96151f5964114e577a095c4
Lakshmi Sashank Gandodi <sashank.gandodi@copart.com>
2024-03-14 21:54:56 +0530

    Finished First Draft

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,28 @@ const addTask = (task) => {
     isEditing: null,
   });
 };
+
+const toggleTaskCompletion = (taskIndex) => {
+  allTasks.value[taskIndex].isCompleted =
+    !allTasks.value[taskIndex].isCompleted;
+};
+
+const editTask = (taskIndex) => {
+  allTasks.value[taskIndex].isEditing = !allTasks.value[taskIndex].isEditing;
+};
+
+const updateTask = (taskIndex, taskValue) => {
+  allTasks.value[taskIndex].task = taskValue;
+};
+
+const deleteTask = (taskId) => {
+  allTasks.value = allTasks.value.filter((task) => task.id !== taskId);
+};
+
+const saveToLocal = () => {
+  localStorage.setItem("all-tasks", JSON.stringify(allTasks.value));
+};
+
 </script>
 
 <template>
@@ -21,7 +43,15 @@ const addTask = (task) => {
     <h1 class="text-center text-xl font-bold text-primary">Create a Task</h1>
     <CreateTask @new-task="addTask" />
     <ul v-if="allTasks.length > 0">
-      <Task v-for="task in allTasks" :task="task" />
+      <Task
+        v-for="(task, index) in allTasks"
+        :task="task"
+        :index="index"
+        @toggle-complete="toggleTaskCompletion"
+        @edit-task="editTask"
+        @update-task="updateTask"
+        @delete-task="deleteTask"
+      />
     </ul>
     <p v-else>No Tasks</p>
   </main>
--- src/views/Task.vue
+++ src/views/Task.vue
@@ -6,24 +6,36 @@ const props = defineProps({
     type: Object,
     required: true,
   },
+  index: {
+    type: Number,
+    required: true,
+  },
 });
 
-const emit = defineEmits(['mark-complete'])
-const markComplete = () => {
-  task.isCompleted = true
-  emit('mark-complete')
-}
+defineEmits(["toggle-complete", "edit-task", "update-task", "delete-task"]);
 </script>
 
 <template>
   <li>
-    <input type="checkbox" :checked="markComplete" />
+    <input
+      type="checkbox"
+      :checked="task.isCompleted"
+      @input="$emit('toggle-complete', index)"
+    />
     <div class="flex">
-      <input v-if="task.isEditing" type="text" :value="task.task" class="border" />
-      <div v-else="task.task">{{ task.task }}</div>
+      <input
+        v-if="task.isEditing"
+        type="text"
+        :value="task.task"
+        class="border"
+        @input="$emit('update-task', index, $event.target.value)"
+      />
+      <div v-else="task.task" :class="task.isCompleted && 'line-through'">
+        {{ task.task }}
+      </div>
       <CheckBadgeIcon @click="task.isEditing = false" on class="size-6" />
-      <TrashIcon class="size-6" />
-      <PencilIcon @click="task.isEditing = true" class="size-6" />
+      <TrashIcon @click="$emit('delete-task', task.id)" class="size-6" />
+      <PencilIcon @click="$emit('edit-task', index)" class="size-6" />
     </div>
   </li>
 </template>
